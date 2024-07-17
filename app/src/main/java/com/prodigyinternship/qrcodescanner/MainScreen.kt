9e6fa4c64aa3bbd49758a5c375dac5fe3e6ce9ff
[Ashking1956] <[ashishnirmal.cse21@aii.ac.in]>
2024-07-17 17:16:23 +0530

    Add Redireact Feature.

--- app/src/main/java/com/prodigyinternship/qrcodescanner/MainScreen.kt
+++ app/src/main/java/com/prodigyinternship/qrcodescanner/MainScreen.kt
@@ -1,6 +1,8 @@
 package com.prodigyinternship.qrcodescanner
 
 import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import android.util.Log
 import android.view.ViewGroup
 import androidx.camera.core.CameraSelector
@@ -16,8 +18,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -30,6 +36,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -37,6 +44,7 @@ import androidx.core.content.ContextCompat
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberPermissionState
 import com.google.common.util.concurrent.ListenableFuture
+import com.google.mlkit.vision.barcode.common.Barcode
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -45,8 +53,7 @@ import java.util.concurrent.Executors
 fun MainScreen() {
     Column(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
+            .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
@@ -62,84 +69,118 @@ fun MainScreen() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CameraPreview() {
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
     var preview by remember { mutableStateOf<Preview?>(null) }
     val barCodeVal = remember { mutableStateOf("") }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(5.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        AndroidView(
-            factory = { ctx ->
-                PreviewView(ctx).apply {
-                    scaleType = PreviewView.ScaleType.FILL_CENTER
-                    layoutParams = ViewGroup.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.MATCH_PARENT
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "QR Code Scanner",
+                        style = MaterialTheme.typography.headlineMedium.copy(
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold,
+                            fontFamily = FontFamily.Serif,
+                            color = Color.White,
+                            letterSpacing = 1.sp,
+                            textAlign = TextAlign.Center
+                        ),
                     )
-                    implementationMode = PreviewView.ImplementationMode.COMPATIBLE
-                }
-            },
+                }, colors = TopAppBarColors(
+                    containerColor = Color(0xFFa2d2ff),
+                    titleContentColor = if (isSystemInDarkTheme()) Color.White else Color.Black,
+                    actionIconContentColor = if (isSystemInDarkTheme()) Color.White else Color.Black,
+                    scrolledContainerColor = if (isSystemInDarkTheme()) Color.White else Color.Black,
+                    navigationIconContentColor = if (isSystemInDarkTheme()) Color.White else Color.Black
+                )
+            )
+        },
+
+        ) {
+        Column(
             modifier = Modifier
-                .size(500.dp)
-                .aspectRatio(1f)
-                .padding(16.dp),
-            update = { previewView ->
-                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-                val cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
-                val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> =
-                    ProcessCameraProvider.getInstance(context)
-
-                cameraProviderFuture.addListener({
-                    preview = Preview.Builder().build().also {
-                        it.setSurfaceProvider(previewView.surfaceProvider)
+                .fillMaxSize()
+                .padding(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            AndroidView(
+                factory = { ctx ->
+                    PreviewView(ctx).apply {
+                        scaleType = PreviewView.ScaleType.FILL_CENTER
+                        layoutParams = ViewGroup.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT
+                        )
+                        implementationMode = PreviewView.ImplementationMode.COMPATIBLE
                     }
+                },
+                modifier = Modifier
+                    .size(500.dp)
+                    .aspectRatio(1f)
+                    .padding(vertical = 8.dp),
+                update = { previewView ->
+                    val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+                    val cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
+                    val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> =
+                        ProcessCameraProvider.getInstance(context)
 
-                    val cameraProvider = cameraProviderFuture.get()
-                    val barcodeAnalyser = BarCodeAnalyser { barcodes ->
-                        barcodes.forEach { barcode ->
-                            barcode.rawValue?.let { barcodeValue ->
-                                barCodeVal.value = barcodeValue
-                                Log.d(
-                                    "Result",
-                                    "CameraPreview: QR Scanned Successfully $barcodeValue"
-                                )
+                    cameraProviderFuture.addListener({
+                        preview = Preview.Builder().build().also {
+                            it.setSurfaceProvider(previewView.surfaceProvider)
+                        }
+
+                        val cameraProvider = cameraProviderFuture.get()
+                        val barcodeAnalyser = BarCodeAnalyser { barcodes ->
+                            barcodes.forEach { barcode ->
+                                barcode.rawValue?.let { barcodeValue ->
+                                    barCodeVal.value = barcodeValue
+                                    Log.d(
+                                        "Result",
+                                        "CameraPreview: QR Scanned Successfully $barcodeValue"
+                                    )
+                                    if (barcode.valueType == Barcode.TYPE_URL) {
+                                        val intent =
+                                            Intent(Intent.ACTION_VIEW, Uri.parse(barcodeValue))
+                                        context.startActivity(intent)
+                                    }
+                                }
                             }
                         }
-                    }
-                    val imageAnalysis = ImageAnalysis.Builder()
-                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                        .build()
-                        .also {
-                            it.setAnalyzer(cameraExecutor, barcodeAnalyser)
-                        }
+                        val imageAnalysis = ImageAnalysis.Builder()
+                            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                            .build()
+                            .also {
+                                it.setAnalyzer(cameraExecutor, barcodeAnalyser)
+                            }
 
-                    try {
-                        cameraProvider.unbindAll()
-                        cameraProvider.bindToLifecycle(
-                            lifecycleOwner, cameraSelector, preview, imageAnalysis
-                        )
-                    } catch (e: Exception) {
-                        Log.e("CameraPreview", "Binding failed", e)
-                    }
-                }, ContextCompat.getMainExecutor(context))
-            }
-        )
-        Text(
-            text = barCodeVal.value,
-            modifier = Modifier.padding(5.dp),
-            style = MaterialTheme.typography.bodyMedium.copy(
-                fontSize = 18.sp,
-                fontFamily = FontFamily.Monospace,
-                fontWeight = FontWeight.Bold,
-                color = if (isSystemInDarkTheme()) Color.White else Color.Black
+                        try {
+                            cameraProvider.unbindAll()
+                            cameraProvider.bindToLifecycle(
+                                lifecycleOwner, cameraSelector, preview, imageAnalysis
+                            )
+                        } catch (e: Exception) {
+                            Log.e("CameraPreview", "Binding failed", e)
+                        }
+                    }, ContextCompat.getMainExecutor(context))
+                }
             )
-        )
+            Text(
+                text = barCodeVal.value,
+                modifier = Modifier.padding(5.dp),
+                style = MaterialTheme.typography.bodyMedium.copy(
+                    fontSize = 18.sp,
+                    fontFamily = FontFamily.Monospace,
+                    fontWeight = FontWeight.Bold,
+                    color = if (isSystemInDarkTheme()) Color.White else Color.Black
+                )
+            )
+        }
     }
 }
